bb98d99f2eda4ed59d0c11e879a5b89d7e4cee9c
solaire <contact@unharmfulhazard.com>
2024-06-04 18:48:16 -0300

    finished messing up with fmt package

--- 3. Printing and Formatting Strings/main.go
+++ 3. Printing and Formatting Strings/main.go
@@ -13,5 +13,16 @@ func main() {
 	fmt.Println("goodbye dudes!")
 
 	age := 35
+	name := "shrek"
 	fmt.Println("my age is", age)
+	fmt.Println("name is", name)
+
+	fmt.Printf("my name is %v and I am %v\n", name, age)
+	fmt.Printf("my name is %q and I am %q\n", name, age)
+	fmt.Printf("my name is %T and I am %T\n", name, age)
+	fmt.Printf("you scored %0.2f points!\n", 255.5555)
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and I am %v\n", name, age)
+	fmt.Println(str)
 }
