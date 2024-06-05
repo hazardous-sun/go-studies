2177c1846f2a2c852f5633669732c0a746a63446
solaire <contact@unharmfulhazard.com>
2024-06-05 00:03:54 -0300

    with great power comes great SEGFAULT errors, but we do not seem to care about it in these lands

---
+++ 10. Package Scope/greetings.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var points = []int{10, 20, 30, 40}
+
+func greetingsFromPackage(n string) {
+	fmt.Println("hello, dear", n)
+}
---
+++ 10. Package Scope/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	greetingsFromPackage("donald duck")
+
+	for _, v := range points {
+		fmt.Println("aberration call to", v)
+	}
+}
